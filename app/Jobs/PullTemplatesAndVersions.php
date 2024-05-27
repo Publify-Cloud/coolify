d6b9bc96225068317e9a00cc66cd5832071bede9
DeVoresyah Arest <de.voresyah@gmail.com>
2024-05-27 22:03:06 +0700

    fix: change to publify cdn

--- app/Jobs/PullTemplatesAndVersions.php
+++ app/Jobs/PullTemplatesAndVersions.php
@@ -26,7 +26,7 @@ class PullTemplatesAndVersions implements ShouldQueue, ShouldBeEncrypted
         try {
             if (!isDev() && !isCloud()) {
                 ray('PullTemplatesAndVersions versions.json');
-                $response = Http::retry(3, 1000)->get('https://cdn.coollabs.io/coolify/versions.json');
+                $response = Http::retry(3, 1000)->get('https://cdn.publify.justahost.cloud/versions.json');
                 if ($response->successful()) {
                     $versions = $response->json();
                     File::put(base_path('versions.json'), json_encode($versions, JSON_PRETTY_PRINT));
